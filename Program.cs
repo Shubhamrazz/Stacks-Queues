71670c584d3c3305252cd14f4d39cf6d84409fb1
Shubhamrazz <sr99845@gmail.com>
2022-05-22 14:32:29 +0530

    [Shubham]Ability to Peep And Pop From The Stack Till It Is Empty

--- Program.cs
+++ Program.cs
@@ -8,13 +8,22 @@ namespace Stacks_Queque_Program
         {
             //Welcome Message
             Console.WriteLine("-----Welcome To The Stacks And Queues Program-----");
-            //Calling Class and Adding Data in Stack
+            Console.WriteLine("----------------------------------------");
+
+           //Calling Class and Adding Data in Stack
             LinkedListStack linkedListStack = new LinkedListStack();
             linkedListStack.Push(70);
             linkedListStack.Push(30);
             linkedListStack.Push(56);
-            //Display Message
-            linkedListStack.Display();
+            Console.WriteLine("--------------------------------------");
+
+            //Peek Value from Stack
+            linkedListStack.Peek();
+            Console.WriteLine("----------------------------");
+            linkedListStack.Pop();
+
+            //Pop value till Stack will empty
+            linkedListStack.IsEmpty();
             Console.ReadLine();
         }
     }
