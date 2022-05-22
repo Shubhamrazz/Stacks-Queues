fe888ad946bc6ec4012798cec24cdd756c81ef94
Shubhamrazz <sr99845@gmail.com>
2022-05-22 14:59:49 +0530

    [Shubham]Ability to Create a Queue of 56->30->70

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace Stacks_Queque_Program
             //Welcome Message
             Console.WriteLine("-----Welcome To The Stacks And Queues Program-----");
             Console.WriteLine("----------------------------------------");
-
+          /*  
            //Calling Class and Adding Data in Stack
             LinkedListStack linkedListStack = new LinkedListStack();
             linkedListStack.Push(70);
@@ -21,9 +21,19 @@ namespace Stacks_Queque_Program
             linkedListStack.Peek();
             Console.WriteLine("----------------------------");
             linkedListStack.Pop();
-
             //Pop value till Stack will empty
             linkedListStack.IsEmpty();
+            */
+
+            //Calling Class and Adding Data in Stack
+            LinkedListQueue linkedListQueue = new LinkedListQueue();
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(70);
+            Console.WriteLine("----------------");
+
+            //Display Queue
+            linkedListQueue.Display();
             Console.ReadLine();
         }
     }
