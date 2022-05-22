ff59c18beab6f730e936a50477015dc34b239409
Shubhamrazz <sr99845@gmail.com>
2022-05-22 13:47:16 +0530

    [Shubham]Ability to create a Stake 56->30->70

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Stacks_Queque_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcome Message
+            Console.WriteLine("-----Welcome To The Stacks And Queues Program-----");
+            //Calling Class and Adding Data in Stack
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            //Display Message
+            linkedListStack.Display();
+            Console.ReadLine();
+        }
+    }
+}
